a9b63722226bb41c2c5dbc31c48df548625257f1
DJWOMS <socanime@gmail.com>
2020-04-14 21:50:55 +0300

    update user model

--- src/app/components/user/models.py
+++ src/app/components/user/models.py
@@ -6,10 +6,14 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True, unique=True)
-    name = Column(String, unique=True)
+    username = Column(String, unique=True)
     email = Column(String, unique=True)
     password = Column(String)
-    date = Column(DateTime)
+    first_name = Column(String(150))
+    last_name = Column(String(150))
+    date_join = Column(DateTime)
+    last_login = Column(DateTime)
     is_active = Column(Boolean, default=False)
     is_admin = Column(Boolean, default=False)
+    avatar = Column(String)
 
