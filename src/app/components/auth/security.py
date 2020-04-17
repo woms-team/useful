1bdc9b50dfce43fd2a6a6dff7899024cf7e11a52
DJWOMS <socanime@gmail.com>
2020-04-17 21:00:08 +0300

    add auth

--- src/app/components/auth/security.py
+++ src/app/components/auth/security.py
@@ -1,5 +1,6 @@
 from passlib.context import CryptContext
 
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
--- src/app/routers.py
+++ src/app/routers.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
-
+from src.app.components.auth import api as auth_api
 
 api_router = APIRouter()
+
+api_router.include_router(auth_api.router, tags=["login"])
