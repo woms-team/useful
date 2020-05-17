68045443dcf852b62d37363a5aa7b649de744cc4
DJWOMS <socanime@gmail.com>
2020-05-17 13:15:58 +0300

    add models blog

---
+++ src/app/blog/models.py
@@ -0,0 +1,75 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, sql, Table
+from sqlalchemy.orm import relationship, backref
+
+from src.db.session import Base
+
+
+class Category(Base):
+    """Класс модели категорий сетей"""
+    __tablename__ = 'blog_categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    published = Column(Boolean, default=True)
+    description = Column(String(300))
+    parent_id = Column(Integer, ForeignKey('blog_categories.id', ondelete="SET NULL"), nullable=True)
+    children = relationship("Category", backref=backref('parent', remote_side=[id]))
+
+
+tags = Table(
+    'tags',
+    Base.metadata,
+    Column('post_id', Integer, ForeignKey('blog_posts.id')),
+    Column('tag_id', Integer, ForeignKey('blog_tags.id'))
+)
+
+
+class Tag(Base):
+    """Класс модели тегов"""
+    __tablename__ = 'blog_tags'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    post = relationship("Post", secondary=tags, back_populates="posts")
+
+
+class Post(Base):
+    """Класс модели новости"""
+    __tablename__ = 'blog_posts'
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey("user.id", ondelete="SET NULL"))
+    category_id = Column(Integer, ForeignKey('blog_categories.id', ondelete="SET NULL"))
+    title = Column(String(500))
+    mini_text = Column(String(50000))
+    text = Column(String)
+    created_date = Column(DateTime(timezone=True), server_default=sql.func.now())
+    published_date = Column(DateTime, server_default=sql.func.now())
+    image = Column(String, nullable=True)
+    published = Column(Boolean, default=True)
+    viewed = Column(Integer, default=0)
+    description = Column(String(300))
+
+    author = relationship("User", back_populates="posts")
+    category = relationship("Category", back_populates="posts")
+    tag = relationship("Tag", secondary=tags, back_populates="tags")
+
+
+class Comment(Base):
+    """Модель коментариев к новостям"""
+    __tablename__ = 'blog_comments'
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
+    post = Column(Integer, ForeignKey("blog_posts.id", ondelete="CASCADE"))
+    parent_id = Column(Integer, ForeignKey('blog_comments.id'), nullable=True)
+    children = relationship('Comment', backref=backref('parent', remote_side=[id]), lazy='dynamic')
+    message = Column(String(2000))
+    created_date = Column(DateTime(timezone=True), server_default=sql.func.now())
+    update_date = Column(DateTime(timezone=True), server_default=sql.func.now())
+    published = Column(Boolean, default=True)
+    deleted = Column(Boolean, default=False)
+
+
+class SpySearch(Base):
+    """Модель отслеживания запросов поиска"""
+    __tablename__ = 'blog_search'
+    id = Column(Integer, primary_key=True)
+    record = Column(String(1000))
+    counter = Column(Integer, default=0)
--- src/db/base.py
+++ src/db/base.py
@@ -1,5 +1,5 @@
 from src.db.session import Base
-# from src.apps.blog.models import Category, Post, Tag
 from src.app.user.models import User
+from src.app.blog.models import Category, Post, Tag, Comment, SpySearch
 # from src.app.board.models import Category
 from src.app.auth.models import Verification
